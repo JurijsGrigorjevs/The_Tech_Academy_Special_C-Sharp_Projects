60a36a710dbf0e2d43d78f631cb38e24c186f2e7
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-07-23 12:05:55 +0100

    VariablesAndDataTypes
    VariablesAndDataTypes

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.394993939;
+
+            float secondsLeft = 2.43f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Jurijs";
+
+            int currentAge = 23;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+        }
+    }
+}

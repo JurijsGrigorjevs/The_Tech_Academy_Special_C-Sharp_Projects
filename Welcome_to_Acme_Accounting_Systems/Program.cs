1ba5e5b407ba5bcf8949a41e0ebb25ae91c60db3
yurijg@hotmail.com <yurijg@hotmail.com>
2024-07-25 00:40:24 +0100

    add

--- Welcome_to_Acme_Accounting_Systems/Program.cs
+++ Welcome_to_Acme_Accounting_Systems/Program.cs
@@ -10,36 +10,36 @@ class Program
         Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're  \"accounting\" on you!");
         Console.ReadLine();
 
-         // Addition
- int num1 = 75;
- int num2 = 88;
- int total = num1 + num2;
- Console.WriteLine(total);
+                     // Addition
+             int num1 = 75;
+             int num2 = 88;
+             int total = num1 + num2;
+             Console.WriteLine(total);
 
- // Subtractio
- int num3 = 45;
- int num4 = 55;
- int difference = num3 - num4;
- Console.WriteLine(difference);
+             // Subtractio
+             int num3 = 45;
+             int num4 = 55;
+             int difference = num3 - num4;
+             Console.WriteLine(difference);
 
- // Multiplication
- int num5 = 99;
- int num6 = 33;
- int product = num5 * num6;
- Console.WriteLine(product);
+             // Multiplication
+             int num5 = 99;
+             int num6 = 33;
+             int product = num5 * num6;
+             Console.WriteLine(product);
 
- // Division
- int num7 = 81;
- int num8 = 8;
- int quotient = num7 / num8;
- Console.WriteLine(quotient);
+             // Division
+             int num7 = 81;
+             int num8 = 8;
+             int quotient = num7 / num8;
+             Console.WriteLine(quotient);
 
-    int num9 = 7;
-   float num10 = 5.5f;
-   float total1 = num9 + num10;
-   int total2 = num9 + (Int32)num10;
-   Console.WriteLine(total1);
-   Console.WriteLine(total2);
+                int num9 = 7;
+               float num10 = 5.5f;
+               float total11 = num9 + num10;
+               int total12 = num9 + (Int32)num10;
+               Console.WriteLine(total11);
+               Console.WriteLine(total12);
     }
 }
 

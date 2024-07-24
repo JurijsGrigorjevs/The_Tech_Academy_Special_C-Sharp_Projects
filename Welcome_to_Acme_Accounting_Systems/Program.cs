4f781d5fcc81d77632a42d7d9e9789987431f1d3
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-07-24 13:39:31 +0100

    Update Program.cs
    Add

--- Welcome_to_Acme_Accounting_Systems/Program.cs
+++ Welcome_to_Acme_Accounting_Systems/Program.cs
@@ -9,6 +9,30 @@ class Program
     {
         Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're  \"accounting\" on you!");
         Console.ReadLine();
+
+         // Addition
+ int num1 = 75;
+ int num2 = 88;
+ int total = num1 + num2;
+ Console.WriteLine(total);
+
+ // Subtractio
+ int num3 = 45;
+ int num4 = 55;
+ int difference = num3 - num4;
+ Console.WriteLine(difference);
+
+ // Multiplication
+ int num5 = 99;
+ int num6 = 33;
+ int product = num5 * num6;
+ Console.WriteLine(product);
+
+ // Division
+ int num7 = 81;
+ int num8 = 8;
+ int quotient = num7 * num8;
+ Console.WriteLine(quotient);
     }
 }
 

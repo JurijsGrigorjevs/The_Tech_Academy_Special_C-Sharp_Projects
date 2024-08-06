d6044deebcc1cc31ea5138ff8aa746869f0e7bd7
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-08-06 22:39:20 +0100

    BranchingAssignment
    Your job is to create a console-based application for getting a shipping quote for a package.
    
    Requirements
    
    The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
    
    The user must then be prompted for the package weight.
    
    If the weight is greater than 50, display the error message, “Package too heavy to be shipped via Package Express. Have a good day.” At this point the program would end.
    
    The user must then be prompted for the package width.
    
    Then the package height.
    
    Then the package length.
    
    If the dimensions total greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program would end.
    
    Next, multiply the three dimensions (height, width, & length) together, and multiply the product by the weight. Finally, divide the outcome by 100.
    
    The result of that calculation is the quote.
    
    Display the quote to the user as a dollar amount.
    
    For example: 
    
    Welcome to Package Express. Please follow the instructions below.
    Please enter the package weight:
    40
    Please enter the package width:
    10
    Please enter the package height:
     12
    Please enter the package length:
     11
    Your estimated total for shipping this package is: $528.00
    Thank you!
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ BranchingAssignment/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+namespace BranchingAssignment
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display the message
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            // The user needs to enter the weight of the package.
+            Console.WriteLine("Please enter the package weight:");
+            // Converts the specified string representation of a number entered by the user to its equivalent double-precision floating-point number.
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            // Check if package is too heavy
+            if (weight > 50)
+            {
+                // End the program if the package is too heavy
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                 
+            }
+            else
+            {
+                // The user needs to enter the package width
+                Console.WriteLine("Please enter the package width:");
+                // Converts the specified string representation of a number entered by the user to its equivalent double-precision floating-point number.
+                double width = Convert.ToDouble(Console.ReadLine());
+
+                // The user needs to enter the package height
+                Console.WriteLine("Please enter the package height:");
+                // Converts the specified string representation of a number entered by the user to its equivalent double-precision floating-point number.
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                // The user needs to enter the package length
+                Console.WriteLine("Please enter the package length:");
+                // Converts the specified string representation of a number entered by the user to its equivalent double-precision floating-point number.
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                // Calculate the sum of package dimensions
+                double dimensionsTotal = width + height + length;
+
+
+                // Check if the sum of package dimensions is too big
+                if (dimensionsTotal > 50)
+                {
+                    // End the program if the sum of package dimensions is too big
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    // Calculate the shipping quote
+                    double quote = (width * height * length * weight) / 100;
+
+                    // Display the shipping quote to the user
+                    Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+                    Console.WriteLine("Thank you!");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+

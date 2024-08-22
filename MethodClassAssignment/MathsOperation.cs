0f1e2431a83f22f5cad1d4d8f65d126584ce83a1
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-08-22 16:14:15 +0100

    Method Class Assignment
    Method Class Assignment

---
+++ MethodClassAssignment/MathsOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MethodClassAssignment
+{
+    // Create a class - MathsOperation
+    class MathsOperation
+    {
+        // Create a void method that takes two integers as parameters.
+        public void MathsOps(int number1, int number2)
+        {
+            // Perform a mathematical operation on the first integer
+            int result = number1 * 8;
+
+            // Display the result and the second integer
+            Console.WriteLine("The result of a mathematical operation on the first integer: " + result);
+            Console.WriteLine("Display the second integer: " + number2);
+        }
+    }
+}

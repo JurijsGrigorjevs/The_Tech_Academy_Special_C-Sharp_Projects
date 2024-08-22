92532b59d9f9fb82f3a6dbcfa77af2a78c1bad3c
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-08-22 13:57:45 +0100

    Method Assignment
    Method Assignment

---
+++ MethodAssignment/MathsOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    // Create a class
+    class MathsOps
+    {
+        // Create a method that takes two integers as parameters. Make one of them optional. 
+        public int MathsOperation(int i, int j = 7)
+        {
+            // Have the method do a math operation and return an integer result
+            int result = i + j;
+            return result;
+        }
+
+    }
+}

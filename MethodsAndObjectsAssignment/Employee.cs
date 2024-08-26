d200abe55fb4026257563b8f8e04169258e0f18a
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-08-26 04:47:59 +0100

    Methods and Objects Assignment
    Methods and Objects Assignment

---
+++ MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    // Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        // Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: “Name: [full name]”.
+        public void SayName()
+        {
+            Console.WriteLine(" Name: " + this.FirstName + " " +  this.LastName);
+        }
+    }
+}

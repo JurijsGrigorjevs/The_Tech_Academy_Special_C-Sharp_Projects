49cec9789d216d67e780b7d0e7579bb3530375f7
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-08-27 01:13:03 +0100

    Abstract_Class_Assignment
    Abstract_Class_Assignment

---
+++ AbstractClassAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    // Create another class called Employee and have it inherit from the Person class
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        // Implement the SayName() method inside of the Employee class
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}

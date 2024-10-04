5165f30754388fb7cad505ef95cd83bd58303b2c
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-10-04 23:15:05 +0100

    Add files via upload

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console
+            Console.WriteLine("The current time is: " + DateTime.Now);
+            // Ask the user for a number
+            Console.WriteLine("\nEnter a number to see what the time will be in that many hours: ");
+            float answer = float.Parse(Console.ReadLine());
+            // Print what time it will be in the amount of hours the user input
+            Console.WriteLine("{0} + {1} hours(s) = {2}", DateTime.Now, answer, DateTime.Now.AddHours(answer));
+            Console.ReadLine();
+        }
+    }
+}

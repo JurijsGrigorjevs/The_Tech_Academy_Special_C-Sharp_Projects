f30fba95ffab37a58f86da24fa57f4f55fe96673
Jurijs <JurijsGrigorjevs@users.noreply.github.com>
2024-10-10 04:42:08 +0100

    ConstructorAssignment
    ConstructorAssignment

---
+++ ConstructorAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a const variable
+            const string constName = "Your name is: ";
+            const string constNum = "Your favorite number is: ";
+            Console.WriteLine("Enter your name or favorite number: ");
+
+            // Create a variable using the var keyword
+            var answer = Console.ReadLine();
+            if (float.TryParse(answer, out float x))
+            {
+                Console.WriteLine(constNum);
+                Chain newChain = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(constName);
+                Chain newChain = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
